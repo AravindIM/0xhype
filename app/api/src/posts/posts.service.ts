b84fc3ef2c41c4ab506491b24f11a841130d2e6c
Aravind I M <aravindim@yahoo.com>
2025-12-27 22:54:53 +0530

    feat(posts): added create posts with array implementation

--- app/api/src/posts/posts.service.ts
+++ app/api/src/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Post } from './interface/post.interface';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -8,7 +9,15 @@ export class PostsService {
     find(id: number): Post | undefined {
         return this.posts.find(post => post.id === id);
     }
+
     findAll(): Post[] {
         return this.posts;
+    }
+
+    create(createPostDto: CreatePostDto) {
+        const lastId = this.posts.length;
+        const newPost: Post = {...createPostDto, id: lastId + 1, date: new Date()}
+        this.posts.push(newPost);
+        return true;
     }
 }
