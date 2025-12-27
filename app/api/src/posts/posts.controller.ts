ff1e2114927790b3266a297f0943336302caffc5
Aravind I M <aravindim@yahoo.com>
2025-12-27 19:24:38 +0530

    feat(posts): added post interface to controller and service

--- app/api/src/posts/posts.controller.ts
+++ app/api/src/posts/posts.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { Post } from './interface/post.interface';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postsService: PostsService) {}
   @Get()
-  fetchPosts(): string {
+  async fetchPosts(): Promise<Post[]> {
     return this.postsService.findAll();
   }
 
   @Get(':id')
-  fetchPost(@Param('id', ParseIntPipe) id: number): string{
+  async fetchPost(@Param('id', ParseIntPipe) id: number): Promise<Post> {
     return this.postsService.find(id);
   }
 }
