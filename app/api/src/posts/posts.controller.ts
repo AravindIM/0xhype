3e222feab512e7b3fc7e5e6dfb73716da2a67478
Aravind I M <aravindim@yahoo.com>
2025-12-27 22:54:53 +0530

    feat(posts): added create posts with array implementation

--- app/api/src/posts/posts.controller.ts
+++ app/api/src/posts/posts.controller.ts
@@ -1,21 +1,30 @@
-import { Controller, Get, NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, InternalServerErrorException, NotFoundException, Param, ParseIntPipe, Body } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { Post } from './interface/post.interface';
+import { Post as PostInterface } from './interface/post.interface';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postsService: PostsService) {}
   @Get()
-  async fetchPosts(): Promise<Post[]> {
+  async fetchPosts(): Promise<PostInterface[]> {
     return this.postsService.findAll();
   }
 
   @Get(':id')
-  async fetchPost(@Param('id', ParseIntPipe) id: number): Promise<Post> {
-    const posts: Post | undefined = this.postsService.find(id);
+  async fetchPost(@Param('id', ParseIntPipe) id: number): Promise<PostInterface> {
+    const posts: PostInterface | undefined = this.postsService.find(id);
     if (!posts) {
       throw new NotFoundException('Post not found!');
     }
     return posts;
+  }
+
+  @Post()
+  async create(@Body() createPostDto: CreatePostDto): Promise<string> {
+    if (!this.postsService.create(createPostDto)) {
+      throw new InternalServerErrorException();
+    }
+    return 'Post Created!';
   }
 }
