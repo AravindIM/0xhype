030c38c847c67df17923406d13c2b804c104689f
Aravind I M <aravindim@yahoo.com>
2025-12-27 19:24:38 +0530

    feat(posts): added post interface to controller and service

--- app/api/src/posts/posts.service.ts
+++ app/api/src/posts/posts.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from './interface/post.interface';
 
 @Injectable()
 export class PostsService {
-    find(id: number): string {
-        return `Viewing post with id ${id}`;
+    find(id: number): Post {
+        const post: Post = {
+            id,
+            title: `Sample Post ${id}`,
+            link: `https://example.com/posts/${id}`,
+            date: new Date(),
+        };
+        return post;
     }
-    findAll(): string {
-        return 'Viewing all posts';
+    findAll(): Post[] {
+        const posts: Post[] = [
+            {
+                id: 1,
+                title: "Sample Post 1",
+                link: "https://example.com/posts/1",
+                date: new Date(),
+            },
+            {
+                id: 2,
+                title: "Sample Post 2",
+                link: "https://example.com/posts/2",
+                date: new Date(),
+            }
+        ];
+        return posts;
     }
 }
