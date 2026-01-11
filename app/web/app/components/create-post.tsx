bbfe1d71f740b1552347c1a494fb6b18bff02d44
Aravind I M <aravindim@yahoo.com>
2026-01-12 00:33:59 +0530

    feat(create post component): added props to handle submitting data

--- app/web/app/components/create-post.tsx
+++ app/web/app/components/create-post.tsx
@@ -3,28 +3,44 @@ import type React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-export function CreatePost({ ...props }: React.ComponentProps<"div">) {
+interface CreatePostProps {
+  titleInputProps: React.InputHTMLAttributes<HTMLInputElement>;
+  linkInputProps: React.InputHTMLAttributes<HTMLInputElement>;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+}
+
+export function CreatePost({
+  titleInputProps,
+  linkInputProps,
+  onSubmit,
+}: CreatePostProps) {
   return (
-    <Card className="gap-0!" {...props}>
-      <CardHeader>
-        <CardTitle>
+    <Card className="gap-0!">
+      <form onSubmit={onSubmit}>
+        <CardHeader>
+          <CardTitle>
+            <Input
+              type="text"
+              placeholder="Give the link a title"
+              className="rounded-none! border-0! focus-visible:ring-0! focus-visible:ring-offset-0! shadow-none!"
+              {...titleInputProps}
+            />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
           <Input
             type="text"
-            placeholder="Give the link a title"
+            placeholder="Paste your link here"
             className="rounded-none! border-0! focus-visible:ring-0! focus-visible:ring-offset-0! shadow-none!"
+            {...linkInputProps}
           />
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Input
-          type="text"
-          placeholder="Paste your link here"
-          className="rounded-none! border-0! focus-visible:ring-0! focus-visible:ring-offset-0! shadow-none!"
-        />
-        <div className="flex justify-end pt-6">
-          <Button className="rounded-full">Post</Button>
-        </div>
-      </CardContent>
+          <div className="flex justify-end pt-6">
+            <Button type="submit" className="rounded-full">
+              Post
+            </Button>
+          </div>
+        </CardContent>
+      </form>
     </Card>
   );
 }
