9a74db4b9151b6dfe29c3fca0e936e9290b26ecd
Aravind I M <aravindim@yahoo.com>
2026-01-11 18:02:27 +0530

    chore(rebranding): rebranding applied to api app service

--- app/api/src/app.service.ts
+++ app/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHome(): string {
-    return 'Hype0 API Service';
+    return '0xhype API Service';
   }
 }
