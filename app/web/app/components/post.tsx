7cff922953dacfab3badff80bb4a21e6e397cfae
Aravind I M <aravindim@yahoo.com>
2026-01-19 23:22:32 +0530

    feat(post component): added preview component

--- app/web/app/components/post.tsx
+++ app/web/app/components/post.tsx
@@ -1,20 +1,36 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type React from "react";
+import { Link } from "react-router";
 import { cn } from "~/lib/utils";
+import { Preview, type PreviewProps } from "./preview";
 
-interface PostProps extends React.ComponentProps<"div"> {
+export interface PostProps extends React.ComponentProps<"div"> {
   title: string;
   link: string;
+  preview?: PreviewProps;
 }
 
-export function Post({ title, link, ...props }: PostProps) {
+export function Post({ title, link, preview, ...props }: PostProps) {
   return (
     <Card {...props}>
-      <CardHeader>
+      <CardHeader className="flex flex-row items-center">
         <CardTitle>{title}</CardTitle>
       </CardHeader>
-      <CardContent>
-        <p>{link}</p>
+      <CardContent className="px-0">
+        {preview ? (
+          <Preview
+            title={preview.title}
+            description={preview.description}
+            image={preview.image}
+            siteName={preview.siteName}
+            siteUrl={preview.siteUrl}
+            favicon={preview.favicon}
+          />
+        ) : (
+          <Link to={link} className="text-blue-500 px-6">
+            {link}
+          </Link>
+        )}
       </CardContent>
     </Card>
   );
