1d480b5a4aa1b9453174227f8592927de6166cd1
Aravind I M <aravindim@yahoo.com>
2026-01-19 23:22:09 +0530

    feat(web): added preview component

---
+++ app/web/app/components/preview.tsx
@@ -0,0 +1,64 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export interface PreviewProps {
+  title?: string;
+  description?: string;
+  image?: string;
+  siteName?: string;
+  siteUrl?: string;
+  favicon?: string;
+}
+
+export function Preview({
+  title,
+  description,
+  image,
+  siteName,
+  siteUrl,
+  favicon,
+}: PreviewProps) {
+  const hasPreviewImage: boolean = Boolean(image);
+  const hasPreviewHeader: boolean = Boolean(title);
+  const hasPreviewBody: boolean = Boolean(description);
+  const hasPreviewFooter = Boolean(favicon || siteName || siteUrl);
+  const hasPreviewFooterText = Boolean(siteName || siteUrl);
+  const hasPreviewFooterSeparator = Boolean(siteName && siteUrl);
+
+  return (
+    <>
+      {hasPreviewImage && <img src={image} className="w-full" />}
+      <Card className="gap-1 px-3">
+        {hasPreviewHeader && (
+          <CardHeader className="px-3">
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+        )}
+        {hasPreviewBody && (
+          <CardContent className="px-3 text-muted-foreground font-medium">
+            {description}
+          </CardContent>
+        )}
+        {hasPreviewFooter && (
+          <CardFooter className="px-3 pt-1 gap-3 flex flex-row items-center">
+            {favicon && (
+              <img src={favicon} className="h-4 w-4 shrink-0 object-contain" />
+            )}
+            {hasPreviewFooterText && (
+              <div className="text-muted-foreground font-medium">
+                {siteName}
+                {hasPreviewFooterSeparator && <span> &#8226; </span>}
+                {siteUrl}
+              </div>
+            )}
+          </CardFooter>
+        )}
+      </Card>
+    </>
+  );
+}
