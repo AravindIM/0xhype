9d406741026451e1640d1d3d9c6416bf1dc1c8cd
Aravind I M <aravindim@yahoo.com>
2026-01-26 01:09:31 +0530

    feat(create post component): added hasLink state to track if the link input is empty

--- app/web/app/components/create-post.tsx
+++ app/web/app/components/create-post.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface CreatePostProps {
   titleInputProps: React.InputHTMLAttributes<HTMLInputElement>;
@@ -14,6 +15,7 @@ export function CreatePost({
   linkInputProps,
   onSubmit,
 }: CreatePostProps) {
+  const [hasLink, setHasLink] = useState(false);
   return (
     <Card className="gap-0! pt-2 pb-3">
       <form onSubmit={onSubmit}>
@@ -33,6 +35,7 @@ export function CreatePost({
             placeholder="Drop your link here to share it with the world..."
             className="rounded-none! border-0! focus-visible:ring-0! focus-visible:ring-offset-0! shadow-none! leading-none! p-0 font-normal text-sm"
             {...linkInputProps}
+            onChange={(e) => setHasLink(Boolean(e.target.value.trim()))}
           />
           <div className="flex justify-end p-0">
             <Button type="submit" className="rounded-full px-12">
