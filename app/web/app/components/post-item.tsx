cb393026d5f1e9891ea33d041e66e41d64ad4b52
Aravind I M <aravindim@yahoo.com>
2026-01-26 17:07:13 +0530

    feat(web component): added post item component

---
+++ app/web/app/components/post-item.tsx
@@ -0,0 +1,33 @@
+import type React from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import type { PreviewProps } from "./preview";
+import { Post } from "./post";
+import axios from "axios";
+
+export interface PostItemProps extends React.ComponentProps<"div"> {
+  postid: number;
+  title: string;
+  link: string;
+}
+export function PostItem({ postid, title, link }: PostItemProps) {
+  //   const queryClient = useQueryClient();
+
+  const { isLoading, isError, data } = useQuery<PreviewProps, Error>({
+    queryKey: ["preview", link],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/posts/${postid}/preview`);
+      return data;
+    },
+    retry: false,
+  });
+
+  return (
+    <Post
+      title={title}
+      link={link}
+      preview={data}
+      isPreviewLoading={isLoading}
+      isPreviewError={isError}
+    />
+  );
+}
