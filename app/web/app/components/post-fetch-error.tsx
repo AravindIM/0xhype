f85690ba03b39ff2ccd2255fe8f6ed559f815f5c
Aravind I M <aravindim@yahoo.com>
2026-01-26 13:52:36 +0530

    feat(post fetch error component): added animation to retry button

--- app/web/app/components/post-fetch-error.tsx
+++ app/web/app/components/post-fetch-error.tsx
@@ -1,12 +1,39 @@
 import { RotateCw, WifiOff } from "lucide-react";
 import type React from "react";
 import { Button } from "@/components/ui/button";
+import { useAnimate, motion } from "motion/react";
 
 interface PostFetchErrorProps extends React.ComponentProps<"div"> {
   onRetry: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export function PostFetchError({ onRetry, ...props }: PostFetchErrorProps) {
+  const [scope, animate] = useAnimate();
+  const animateRotate = (onComplete: () => void) => {
+    animate(
+      scope.current,
+      { rotate: 270 },
+      {
+        duration: 0.1,
+        ease: "easeInOut",
+        onComplete: onComplete,
+      },
+    );
+  };
+  const animateReset = (onComplete: () => void) => {
+    animate(
+      scope.current,
+      { rotate: 0 },
+      {
+        duration: 0,
+        ease: "easeInOut",
+        onComplete: onComplete,
+      },
+    );
+  };
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    animateRotate(() => animateReset(() => onRetry(e)));
+  };
   return (
     <div
       className="p-6 w-full flex flex-col justify-center items-center gap-3"
@@ -23,10 +50,13 @@ export function PostFetchError({ onRetry, ...props }: PostFetchErrorProps) {
         </p>
       </div>
       <Button
-        onClick={onRetry}
-        className="rounded-full cursor-pointer gap-3 py-4! px-6! my-4"
+        onClick={handleClick}
+        className="rounded-full cursor-pointer py-4! my-4 flex justify-between"
       >
-        <RotateCw /> Retry
+        <motion.span ref={scope}>
+          <RotateCw data-icon="inline-start" />
+        </motion.span>
+        <div className="px-2">Retry</div>
       </Button>
     </div>
   );
