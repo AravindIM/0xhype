797748b562ff19c319913e85de77e3306ed2beb3
Aravind I M <aravindim@yahoo.com>
2026-01-26 16:30:07 +0530

    fix(post api): rename id to postid

--- app/api/src/posts/posts.controller.ts
+++ app/api/src/posts/posts.controller.ts
@@ -18,13 +18,15 @@ import { LinkPreviewDto } from './dto/link-preview.dto';
 export class PostsController {
   constructor(private postsService: PostsService) {}
   @Get()
-  async fetchPosts(): Promise<PostDto[]> {
+  async fetchPosts(): Promise<PostEntity[]> {
     const posts: PostEntity[] = await this.postsService.findAll();
     return posts;
   }
 
-  @Get(':id')
-  async fetchPost(@Param('id', ParseIntPipe) id: number): Promise<PostDto> {
+  @Get(':postid')
+  async fetchPost(
+    @Param('postid', ParseIntPipe) id: number,
+  ): Promise<PostEntity> {
     const post: PostEntity | null = await this.postsService.find(id);
     if (!post) {
       throw new NotFoundException('Post not found!');
@@ -32,9 +34,9 @@ export class PostsController {
     return post;
   }
 
-  @Get(':id/preview')
+  @Get(':postid/preview')
   async fetchPreview(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('postid', ParseIntPipe) id: number,
   ): Promise<LinkPreviewDto | undefined> {
     const post: PostEntity | null = await this.postsService.find(id);
     if (!post) {
