15d68e9d29a2830a74a0109da6607791a8a4d95f
Aravind I M <aravindim@yahoo.com>
2026-01-26 17:06:56 +0530

    feat(post component): added preview loading and error checking

--- app/web/app/components/post.tsx
+++ app/web/app/components/post.tsx
@@ -3,21 +3,37 @@ import type React from "react";
 import { Link } from "react-router";
 import { cn } from "~/lib/utils";
 import { Preview, type PreviewProps } from "./preview";
+import { PreviewSkeleton } from "./preview-skeleton";
 
 export interface PostProps extends React.ComponentProps<"div"> {
   title: string;
   link: string;
   preview?: PreviewProps;
+  isPreviewLoading: boolean;
+  isPreviewError: boolean;
 }
 
-export function Post({ title, link, preview, ...props }: PostProps) {
+export function Post({
+  title,
+  link,
+  preview,
+  isPreviewLoading = false,
+  isPreviewError = false,
+  ...props
+}: PostProps) {
   return (
     <Card {...props}>
       <CardHeader className="flex flex-row items-center">
         <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardContent className="px-0">
-        {preview ? (
+        {isPreviewLoading ? (
+          <PreviewSkeleton />
+        ) : isPreviewError || !preview ? (
+          <Link to={link} className="text-blue-500 px-6">
+            {link}
+          </Link>
+        ) : (
           <Link to={link} target="_blank" rel="noopener noreferrer">
             <Preview
               title={preview.title}
@@ -27,10 +43,6 @@ export function Post({ title, link, preview, ...props }: PostProps) {
               siteUrl={preview.siteUrl}
               favicon={preview.favicon}
             />
-          </Link>
-        ) : (
-          <Link to={link} className="text-blue-500 px-6">
-            {link}
           </Link>
         )}
       </CardContent>
