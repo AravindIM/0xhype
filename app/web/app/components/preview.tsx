7e51902c702fab86bc87a48adac565bff4e5eb00
Aravind I M <aravindim@yahoo.com>
2026-02-04 23:56:07 +0530

    feat(web component): added preview info component

--- app/web/app/components/preview.tsx
+++ app/web/app/components/preview.tsx
@@ -9,68 +9,41 @@ import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { useImagePreload } from "@/hooks/use-image-preload";
 import { AnimatePresence, motion } from "motion/react";
 import { PreviewImage } from "./preview-image";
+import { PreviewInfo } from "./preview-info";
 
 export interface PreviewProps {
+  link: string;
   title?: string;
   description?: string;
   image?: string;
   siteName?: string;
   siteUrl?: string;
   favicon?: string;
+  isLoading?: boolean;
 }
 
 export function Preview({
+  link,
   title,
   description,
   image,
   siteName,
   siteUrl,
   favicon,
+  isLoading = false,
 }: PreviewProps) {
-  const hasPreviewHeader: boolean = Boolean(title);
-  const hasPreviewBody: boolean = Boolean(description);
-  const hasPreviewFooter = Boolean(favicon || siteName || siteUrl);
-  const hasPreviewFooterText = Boolean(siteName || siteUrl);
-  const hasPreviewFooterSeparator = Boolean(siteName && siteUrl);
-
   return (
     <>
-      <PreviewImage src={image} />
-      <Card className="gap-1 px-3">
-        {hasPreviewHeader && (
-          <CardHeader className="px-3">
-            <CardTitle>{title}</CardTitle>
-          </CardHeader>
-        )}
-        {hasPreviewBody && (
-          <CardContent className="px-3 text-muted-foreground font-medium">
-            {description}
-          </CardContent>
-        )}
-        {hasPreviewFooter && (
-          <CardFooter className="px-3 pt-1 gap-3 flex flex-row items-center">
-            {favicon && (
-              <motion.img
-                src={favicon}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.1, ease: "easeIn" }}
-                className="h-4 w-4 shrink-0 object-contain"
-                onError={(e) => {
-                  e.currentTarget.style.display = "none";
-                }}
-              />
-            )}
-            {hasPreviewFooterText && (
-              <div className="text-muted-foreground font-medium">
-                {siteName}
-                {hasPreviewFooterSeparator && <span> &#8226; </span>}
-                {siteUrl}
-              </div>
-            )}
-          </CardFooter>
-        )}
-      </Card>
+      <PreviewImage src={image} isLoading={isLoading} />
+      <PreviewInfo
+        link={link}
+        title={title}
+        description={description}
+        siteName={siteName}
+        siteUrl={siteUrl}
+        favicon={favicon}
+        isLoading={isLoading}
+      />
     </>
   );
 }
